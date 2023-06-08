2d3454b270637285998a8776e9ea2777259f942c
saurav <developerweb6@gmail.com>
2023-06-08 22:14:07 +0530

    config.js changes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // images: {
-  //   formats: ['image/jpg', 'image/png'],
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'http',
-  //       hostname: 'localhost',
-  //       port: '',
-  //       pathname: '/hotel/public/images/**',
-  //     },
-  //   ],
-  // },
+  images: {
+   domains : 'http://localhost'
+  },
 };
 
 
